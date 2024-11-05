450c22cadd8912147e77b1b42eef67961bd8dd6f
The Glitch <142335587+The-Silly-Glitch@users.noreply.github.com>
2024-11-06 00:17:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './main.scss';
+import './styles.scss';
+import BoardView from "./components/Board";
+
+const App = () => {
+    return <BoardView />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
